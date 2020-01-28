5a81bd05990fcc82ab3e2f44fd21c47b679daea6
Martin Choinski <corky@bigpond.net.au>
2020-01-28 19:21:24 +1100

    first pass before heroku

---
+++ mm_app/static/mm.js
@@ -0,0 +1,130 @@
+
+const max_guess_count = 10;
+const pegs = []; //current set of pegs colors index
+let   guess_count = 0;
+let   result = [];
+
+//set these as desired
+const colors = [
+    "rgb(255, 32, 32)",
+    "rgb(240, 160, 16)",
+    "rgb(240, 240, 32)",
+    "rgb(128, 255, 32)",
+    "rgb(32, 255, 255)",
+    "rgb(32, 32, 255)",
+    "rgb(255, 32, 255)"
+    ];
+
+function start_game(number_of_pegs, number_of_colors) {
+    const start = {
+        number_of_pegs: number_of_pegs, 
+        number_of_colors : number_of_colors
+    };
+
+    $.post("/start", start, (data) => {
+        //console.log(`start_game = [${data}]`);
+    });
+}
+
+function gameMessage() {
+    const numRight = result.reduce((a, b) => a + b, 0);
+    const remaining = max_guess_count - guess_count;
+    let msg = "";
+    if(numRight == pegs.length) {
+        msg = `congrats you have guessed the secret pegs in ${guess_count} ${guess_count==1 ? 'guess':'guesses'}`;
+        end_game();
+    } else if(remaining <= 0) {
+        msg = `sorry you lose, no more guesses available`;
+        end_game();
+    } else {
+        msg = `there are a maximum of ${max_guess_count} guesses.`;
+        msg += ` you have ${remaining} ${remaining==1 ? 'guess':'guesses'} left`;
+    }
+
+    $('#game-msg').text(msg);
+}
+
+function end_game() {
+    $("#guess-btn").hide(100);
+    $("#guess .pegs").hide(100);
+    $("#replay-btn").slideDown(400);
+}
+
+function new_game() {
+    //reuse arrays
+    guess_count = 0;
+    pegs.length = 0; 
+    result.length = 0; 
+
+    //set 1st guess set to a random color pick
+    $("#guess .peg").each(function() {
+        let colorIndex = Math.floor(Math.random()*colors.length);
+        pegs.push(colorIndex);
+        $( this ).css( "background-color", colors[colorIndex]);
+    });
+
+    start_game(pegs.length, colors.length);
+    gameMessage();
+}
+
+function init() {
+    //show current color set in header
+    for(color of colors) {
+        $('<div class="peg"></div>')
+        .css( "background-color", color)
+        .appendTo(".swatch");
+    }
+    //hide replay button
+    $("#replay-btn").hide();
+    
+    new_game();
+}
+
+init();
+
+$("#guess .peg").on("click", function() {
+    const index = $("#guess .peg").index($(this));
+
+    //increment color index for this peg
+    let colorIndex = pegs[index]; 
+    colorIndex++;
+    if(colorIndex >= colors.length) colorIndex = 0;
+    pegs[index] = colorIndex;
+    
+    $( this ).css( "background-color", colors[colorIndex]);
+});
+
+$("#guess").on("submit", function(e) {
+    e.preventDefault();
+    let guessed =`<div class="guess-count">${++guess_count}</div>`; 
+    $.post("/guess",{ pegs: JSON.stringify(pegs) }, (data) => {
+        result = JSON.parse(data);
+        //console.log(result);
+        let match = $('<div class="matches"></div>');
+        let guess = $("#guess .pegs").clone().prepend(guessed);;
+        for(r of result) {
+            if(r==1) {
+                $(`<div class="exact-match"></div>`).appendTo(match);
+            } else if(r==0) {
+                $(`<div class="color-match"></div>`).appendTo(match);
+            }
+        }
+        $("#guess-btn").fadeOut(100).fadeIn(100);
+        match.appendTo(guess);
+        guess.prependTo(".board").hide().slideDown(600);
+
+        gameMessage();
+    });
+});
+
+$("#replay-btn").on("click", function() {
+    $("#replay-btn").slideUp(300);
+    $(".board .pegs").fadeOut(300).remove();
+    $("#guess-btn").slideDown(300);
+    $("#guess .pegs").slideDown(300);
+    new_game();
+});
+
+$(document).on("resize",function(e) {
+    console.log(`size = [${$(this).width()},${$(this).height()}]`)
+}); 
--- mm_app/views.py
+++ mm_app/views.py
@@ -1,10 +1,44 @@
-from flask import Flask, render_template
+import json
+from flask import Flask, render_template, redirect, request
+from random import randint
 from . import app
+
+secret = []
 
 @app.route("/")
 def home():
     return render_template("mm.html")
 
+@app.route("/start", methods=['POST'])
+def start():
+    n_pegs = int(request.form["number_of_pegs"])
+    n_colors = int(request.form["number_of_colors"])
+    
+    secret.clear()
+    for i in range(n_pegs):
+        n = randint(0, n_colors-1)
+        secret.append(n)
+
+    return f'secret colors = {secret}'
+
 @app.route("/guess", methods=['POST'])
-def home():
-    return render_template("mm.html")
+def guess():
+    peg = json.loads(request.form["pegs"])
+    secret_peg = secret.copy()
+    result = []
+    
+    for i in range(len(peg)):
+        if peg[i] == secret_peg[i] :
+            result.append(1)
+            peg[i] = -1
+            secret_peg[i] = -2
+
+    for i in range(len(peg)):
+        for j in range(len(secret_peg)):
+            if peg[i] == secret_peg[j] :
+                result.append(0)
+                peg[i] = -1
+                secret_peg[j] = -2
+                break
+
+    return json.dumps(result)
